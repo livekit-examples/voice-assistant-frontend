bbe44c23ecac94a71dd55dabe7b38423718b8a9a
Jonas Schell <jonas@livekit.io>
2024-09-16 13:45:27 +0200

    remove unused code

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,14 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
