976260207367a02535fba5ca26ee43178c35e298
Husain Topiwala <husain.topiwala@think41.com>
2024-11-26 18:47:52 +0530

    updating to prod urls

--- app/config-url.tsx
+++ app/config-url.tsx
@@ -1,7 +1,7 @@
 const urlConfig = {
-    apiBaseUrl: process.env.NEXT_PUBLIC_HTTPS_URL || "https://fastapi-server-1081098542602.us-central1.run.app",
-    webSocketUrl : process.env.REACT_APP_WS_URL || "wss://jpmc-demo-r2888xe7.livekit.cloud",
-    secretCode: process.env.REACT_APP_SECRET_CODE || "c428661b-e82e-49ec-82a9-a46a16b25382",
+    apiBaseUrl: process.env.NEXT_PUBLIC_HTTPS_URL || "https://askme-server-1081098542602.us-central1.run.app/",
+    webSocketUrl : process.env.REACT_APP_WS_URL || "wss://jpmc-askme-5ykvxg3u.livekit.cloud",
+    secretCode: process.env.REACT_APP_SECRET_CODE || "cdeb49d0-534f-4216-84ed-3261bcc3f7d6",
     getFullUrl: () => {
         if (typeof window === 'undefined') return '';
         return `${window.location.origin}/#/?secret_code=${urlConfig.secretCode}`;
