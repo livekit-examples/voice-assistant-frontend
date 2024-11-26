baab328e15bdac61d3033007b99ea574108235f2
Husain Topiwala <husain.topiwala@think41.com>
2024-11-26 18:33:25 +0530

    update urls for uat

--- app/config-url.tsx
+++ app/config-url.tsx
@@ -1,6 +1,6 @@
 const urlConfig = {
-    apiBaseUrl: process.env.NEXT_PUBLIC_HTTPS_URL || "http://localhost:8000",
-    webSocketUrl : process.env.REACT_APP_WS_URL || "wss://vassistant-a44r3kjn.livekit.cloud",
+    apiBaseUrl: process.env.NEXT_PUBLIC_HTTPS_URL || "https://fastapi-server-1081098542602.us-central1.run.app",
+    webSocketUrl : process.env.REACT_APP_WS_URL || "wss://jpmc-demo-r2888xe7.livekit.cloud",
     secretCode: process.env.REACT_APP_SECRET_CODE || "c428661b-e82e-49ec-82a9-a46a16b25382",
     getFullUrl: () => {
         if (typeof window === 'undefined') return '';
