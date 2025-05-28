bcbc79a57a7fbe2efe929f5190fd4671f28dda5b
ThomFoolery <thomas.yuill@livekit.io>
2025-05-28 11:39:29 -0400

    feat: add branding (#14)

--- app/page.tsx
+++ app/page.tsx
@@ -55,6 +55,27 @@ export default function Home() {
 
   return (
     <div className="flex min-h-svh flex-col justify-center">
+      <header className="absolute top-0 left-0 hidden w-full flex-row justify-between p-6 md:flex">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://livekit.io"
+          className="scale-100 transition-transform duration-300 hover:scale-110"
+        >
+          <img src="/lk-logo.svg" alt="LiveKit Logo" className="size-6" />
+        </a>
+        <span className="text-foreground font-mono text-xs font-bold tracking-wider uppercase">
+          Built with{' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/livekit/agents"
+            className="underline underline-offset-4"
+          >
+            LiveKit Agents
+          </a>
+        </span>
+      </header>
       {sessionStarted ? (
         <RoomContext.Provider value={room}>
           <SessionView />
